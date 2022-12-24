482bbbe393f980c2a7927b623a84b7226bb6279b
Emre Berat Kıran <87636497+EmreBeratKR@users.noreply.github.com>
2022-12-24 03:38:51 +0300

    clean up

--- EmreBeratKR/IdleCash/Test/Test.cs
+++ EmreBeratKR/IdleCash/Test/Test.cs
@@ -5,17 +5,26 @@ public class Test : MonoBehaviour
 {
     private void Start()
     {
-        IdleCash a = new IdleCash(1, "k");
-        IdleCash b = new IdleCash(1, "m");
-        float t1 = 0.5f;
-        float t2 = 2.34f;
+        IdleCash idleCashLhs = new IdleCash(975.1f, "b");
+        IdleCash idleCashRhs = new IdleCash(13.78f, "t");
+        float floatRhs = 101.48f;
+
+        IdleCash add = idleCashLhs + idleCashRhs;
+        IdleCash substract = idleCashLhs - idleCashRhs;
         
+        IdleCash multiply_IdleCash_IdleCash = idleCashLhs * idleCashRhs;
+        IdleCash multiply_IdleCash_float = idleCashLhs * floatRhs;
         
-        // Lerps between "a" to "b" depending on "t1"
-        // "t1" is clamped between 0 and 1
-        IdleCash.Lerp(a, b, t1);
+        IdleCash divide_IdleCash_IdleCash = idleCashLhs / idleCashRhs;
+        IdleCash divide_IdleCash_float = idleCashLhs / floatRhs;
+
+        bool equals = idleCashLhs == idleCashRhs;
+        bool notEquals = idleCashLhs != idleCashRhs;
         
-        // Lerps between "a" to "b" depending on "t2"
-        IdleCash.LerpUnclamped(a, b, t2);
+        bool isSmaller = idleCashLhs < idleCashRhs;
+        bool isGreater = idleCashLhs > idleCashRhs;
+        
+        bool isSmallerOrEqual = idleCashLhs <= idleCashRhs;
+        bool isGreaterOrEqual = idleCashLhs >= idleCashRhs;
     }
 }
